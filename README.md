a92fdf6ebc42b96ce31d915690b1b5fe09e1bfad
juicydrey <my.sabaka@gmail.com>
2017-10-09 22:20:31 -0400

    Changed layout/style and added script to deal with null values

--- README.md
+++ README.md
@@ -17,10 +17,9 @@ My solution has the following features:
 * Return the library to it's original state while showing the full list of books. 
 * On load, show only 10 records of books.
 * Fully responsive layout
-* Retro styling 
 
 # Handling Errors
-The only big issue I ran into while creating the library was while trying to sort the library alphabetically. Since JavaScript is pass-by-value, like Java, when I referenced the original object by calling on the authorDescending and authorAscending functions, the original object would sort as well and when calling on the showFullLibrary function, the library would be sorted depending on the last sorting function that was called. To fix this, I created a function that creates a clone of the global object by calling the constructor function and looping through the object while copying over it's properties into the new cloned object. See below: 
+One of the issues I ran into while creating the library was while trying to sort the library alphabetically. Since JavaScript is pass-by-value, like Java, when I referenced the original object by calling on the authorDescending and authorAscending functions, the original object would sort as well and when calling on the showFullLibrary function, the library would be sorted depending on the last sorting function that was called. To fix this, I created a function that creates a clone of the global object by calling the constructor function and looping through the object while copying over it's properties into the new cloned object. See below: 
 ```
 let cloneObj = (obj) => {
     let clone = obj.constructor();
@@ -42,4 +41,10 @@ let cloneObj = (obj) => {
 - [x] Set method to return depending on decade
 - [x] Refactor Code
 - [x] Add CSS styles
+- [x] Change layout/style
+- [x] Add number of books currently displayed
+- [x] Set 'N/A' for null and empty strings
+- [ ] Sort each column by pressing on the label instead of pressing on buttons. 
+- [ ] Return author values with last name, follwed by first name. 
+- [ ] Refactor code again 
 
--- js/library.js
+++ js/library.js
@@ -5,6 +5,7 @@ var mainLibrary = {};
 xhr.onload = function () {
     if (this.readyState === 4 && this.status === 200) {
         mainLibrary = JSON.parse(this.responseText);
+        cleanFalsyObj(mainLibrary);
         renderData(mainLibrary.slice(0, 10));
     }
 };
@@ -12,26 +13,61 @@ xhr.open('GET', url, true);
 xhr.send(null);
 
 let renderData = (data) => {
-    var container = '';
+    let count = 0;
+    var container =
+        `
+            <table class="library-table">
+                <thead>
+                    <tr class="table-row">
+                        <th id="col-1"><h3>Title</h3></th>
+                        <th id="col-2"><h3>Author</h3></th>
+                        <th id="col-3"><h3>ISBN</h3></th>
+                        <th id="col-4"><h3>Year</h3></th>
+                    </tr>
+                </thead>
+                <tbody>
+            `;
 
     Array.from(data).forEach(obj => {
         var bookCard =
             `
-                <div class="book-card">
-                    <h4>${obj.title}</h4>
-                    <p><span class="desc">By:</span> ${obj.author}</p>
-                    <p><span class="desc">ISBN:</span> ${obj.isbn}</p>
-                    <p id="card-year"><span class="desc">Year:</span> ${obj.year}</p>
-                </div>
-
-            `
+                <tr class="table-row">
+                    <td>${obj.title}</td>
+                    <td> ${obj.author}</td>
+                    <td> ${obj.isbn}</td>
+                    <td> ${obj.year}</td>
+                </tr>
+            `;
         container += bookCard;
+        count++;
     });
 
+    container +=
+        `
+            </tbody>
+        </table>
+        `
+
     document.getElementById('inner-container').innerHTML = container;
+    document.getElementById('book-count').innerHTML = count;
 }
 
-// Event listeners
+let cleanFalsyObj = (arr) => {
+    arr.forEach(obj => {
+        obj.title = cleanFalsyProp(obj.title);
+        obj.author = cleanFalsyProp(obj.author);
+        obj.isbn = cleanFalsyProp(obj.isbn);
+        obj.year = cleanFalsyProp(obj.year);
+    });
+}
+
+let cleanFalsyProp = (prop) => {
+    if (!prop) {
+        return 'N/A';
+    } else {
+        return prop;
+    }
+}
 
 let cloneObj = (obj) => {
     let clone = obj.constructor();
@@ -42,14 +78,14 @@ let cloneObj = (obj) => {
 }
 
 let sortAscending = (first, second) => {
-  return (first < second) ? -1 : (first > second) ? 1 : 0;
+    return (first < second) ? -1 : (first > second) ? 1 : 0;
 }
 
 let sortDescending = (first, second) => {
-  return (first < second) ? 1 : (first > second) ? -1 : 0;
+    return (first < second) ? 1 : (first > second) ? -1 : 0;
 }
 
-let authorAscending = () => {
+let alphabeticallyAscending = () => {
     let sortedLibrary = cloneObj(mainLibrary).sort((a, b) => {
         let first = a.author.toUpperCase();
         let second = b.author.toUpperCase();
@@ -59,7 +95,7 @@ let authorAscending = () => {
     renderData(sortedLibrary);
 }
 
-let authorDescending = () => {
+let alphabeticallyDescending = () => {
     let sortedLibrary = cloneObj(mainLibrary).sort((a, b) => {
         let first = a.author.toUpperCase();
         let second = b.author.toUpperCase();
@@ -72,67 +108,67 @@ let authorDescending = () => {
 let sortNineties = () => {
     let sortedLibrary = mainLibrary.filter(book =>
         (book.year >= 1990 && book.year < 2000));
-    
+
     sortedLibrary.sort((a, b) => {
         let first = a.year;
         let second = b.year;
         return sortAscending(first, second);
     });
-    
+
     renderData(sortedLibrary);
 }
 
 let sortEightes = () => {
     let sortedLibrary = mainLibrary.filter(book =>
         (book.year >= 1980 && book.year < 1990));
-    
+
     sortedLibrary.sort((a, b) => {
         let first = a.year;
         let second = b.year;
         return sortAscending(first, second);
     });
-    
+
     renderData(sortedLibrary);
 }
 
 let sortSeventies = () => {
     let sortedLibrary = mainLibrary.filter(book =>
         (book.year >= 1970 && book.year < 1980));
-    
+
     sortedLibrary.sort((a, b) => {
         let first = a.year;
         let second = b.year;
         return sortAscending(first, second);
     });
-    
+
     renderData(sortedLibrary);
 }
 
 let sortSixtiesPrior = () => {
     let sortedLibrary = mainLibrary.filter(book =>
-        (book.year < 1970 && book.year != false && book.year != null));
-    
+        (book.year < 1970));
+
     sortedLibrary.sort((a, b) => {
         let first = a.year;
         let second = b.year;
         return sortAscending(first, second);
     });
-    
+
     renderData(sortedLibrary);
 }
 
 let sortNA = () => {
-  let sortedLibrary = mainLibrary.filter(book => (book.year == false || book.year == null));
-  renderData(sortedLibrary);
+    let sortedLibrary = mainLibrary.filter(book => (book.year == 'N/A'));
+    renderData(sortedLibrary);
 }
 
 let showfullLibrary = () => {
     renderData(mainLibrary);
 }
 
-document.getElementById('ascending').addEventListener('click',authorAscending);
+document.getElementById('ascending').addEventListener('click', alphabeticallyAscending);
 
-document.getElementById('descending').addEventListener('click', authorDescending);
+document.getElementById('descending').addEventListener('click', alphabeticallyDescending);
 
 document.getElementById('nineties').addEventListener('click', sortNineties);
 
