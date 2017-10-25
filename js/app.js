cce6bbb368e0c89b60bcd400dc2fd62251673992
juicydrey <my.sabaka@gmail.com>
2017-10-25 13:12:58 -0400

    Refactored app.js to use one event listener

--- js/app.js
+++ js/app.js
@@ -4,13 +4,43 @@
     // Main library object that gets rendered to the page
     let mainLibrary = xmlRequest.renderJson(url);
 
-    // Event listeners 
-    document.getElementById('ascending').addEventListener('click', organize.alphabeticallyAscending); 
-    document.getElementById('descending').addEventListener('click', organize.alphabeticallyDescending);
-    document.getElementById('nineties').addEventListener('click', organize.nineties);
-    document.getElementById('eighties').addEventListener('click', organize.eightes);
-    document.getElementById('seventies').addEventListener('click', organize.seventies);
-    document.getElementById('sixties-prior').addEventListener('click', organize.sixtiesPrior);
-    document.getElementById('na').addEventListener('click', organize.NA);
-    document.getElementById('full-library').addEventListener('click', organize.showfullLibrary);
+    // Receives element ID and then calls the appropriate sorting/filter function 
+    let clickButton = (but) => {
+        switch (but) {
+            case 'ascending':
+                organize.alphabeticallyAscending();
+                break;
+            case 'descending':
+                organize.alphabeticallyDescending();
+                break;
+            case 'nineties':
+                organize.nineties();
+                break;
+            case 'eighties':
+                organize.eightes();
+                break;
+            case 'seventies':
+                organize.seventies();
+                break;
+            case 'sixties-prior':
+                organize.sixtiesPrior();
+                break;
+            case 'na':
+                organize.NA();
+                break;
+            case 'full-library':
+                organize.showfullLibrary();
+                break;
+            default:
+                return;
+        };
+    };
+
+    // Single event listener that takes in the target event's ID and calls on clickButton
+    document.querySelector('header').addEventListener('click', (event) => {
+        clickButton(event.target.id);
+    });
+
+
+
 })();
